79b6ad694dd6a95f2a716922c3c3e342e07b755b
swapnil kumar <swapnilk0294@gmail.com>
2024-08-30 12:49:35 +0200

    chore: Update landing page heading text
    The landing page heading text was updated to reflect the correct description of the company's services. This change improves the clarity and accuracy of the message displayed to users.

--- app/page.js
+++ app/page.js
@@ -1,20 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='bg-black max-w-screen overflow-x-hidden min-h-screen flex text-[#FAF9F6] items-center justify-start' >
-      <h1
-      className='text-[7.5rem] font-bold leading-none tracking-tighter text-center'
-      >
-        Vi er et web og app designfirma
-
+    <div className="bg-black max-w-screen overflow-x-hidden min-h-screen flex text-[#FAF9F6] items-center justify-start">
+      <h1 className="md:text-[8.5rem] text-6xl font-bold leading-none tracking-tighter text-left mt-auto mb-48 mx-8">
+        Vi er et web og digital designfirma
       </h1>
-
-
     </div>
-  )
+  );
 }
